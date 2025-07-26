ae7e59f7a32fd3b2d00637e45f94b4827e723ccf
= <=>
2025-07-25 20:37:14 -0400

    this better work

--- src/app/api/extract-text/route.js
+++ src/app/api/extract-text/route.js
@@ -1,6 +1,9 @@
 export const runtime = 'nodejs';
 
-import pdf from 'pdf-parse';
+const pdf = require('pdf-parse');
+
+
+// import pdf from 'pdf-parse';
 import { NextResponse } from 'next/server';
 import TurndownService from 'turndown';
 
